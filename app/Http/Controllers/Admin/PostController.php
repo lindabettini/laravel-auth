5f91b963ee79c88ae0960d734abfd011c430e439
lindabettini <linda.bettini@hotmail.it>
2022-03-29 00:27:32 +0200

    agiunta validazione

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -38,6 +38,19 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|string|unique:posts|min:5|max:50',
+            'content' => 'required|string',
+            'image' => 'url'
+        ], [
+            'title.required' => 'Il titolo è obbligatorio.',
+            'title.min' => 'La lunghezza minima del titolo è di 5 caratteri.',
+            'title.max' => 'La lunghezza massima del titolo è di 50 caratteri.',
+            'title.unique' => "Esiste gia' un post dal titolo ''$request->title''.",
+            'content.required' => 'Scrivi qualcosa nel post.',
+            'image.url' => 'Url immagine non valido.'
+        ]);
+
         $data = $request->all();
         $post = new Post();
         $data['slug'] = Str::slug($request->title, '-'); //vedi alternativa dopo fill
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,26 +5,51 @@
   <h1>Nuovo Post</h1>
 </header>
 <hr>
+
+@if($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
 <form action="{{route('admin.posts.store')}}" method="POST">
   @csrf
   <div class="row">
     <div class="col-12">
       <div class="form-group">
         <label for="title" class="form-label">Titolo Post:</label>
-        <input type="text" name="title" class="form-control" id="title" placeholder="Titolo">
+        <input type="text" name="title" id="title" placeholder="Titolo" value="{{old('title')}}" class="form-control @error('title') is-invalid @enderror">
+        @error('title')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+        @enderror
       </div>
     </div>
     <div class="col-12">
       <div class="form-group">
         <label for="content" class="form-label">Scrivi Post:</label>
-        <textarea class="form-control" name="content" id="content" rows="10"></textarea>
+        <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" rows="10">{{old('content')}}</textarea>
       </div>
+      @error('image')
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
     </div>
     <div class="col-10">
       <div class="form-group">
         <label for="image" class="form-label">Inserisci url immagine:</label>
-        <input type="url" name="image" class="form-control" id="image" placeholder="Url dell'immagine">
+        <input type="url" name="image" class="form-control @error('image') is-invalid @enderror" id="image" placeholder="Url dell'immagine" value="{{old('image')}}">
       </div>
+      @error('image')
+      <div class=" invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
     </div>
 
     <div class="col-2">
