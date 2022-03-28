e8c6b537fc5b5282d36e58dcdbfa13e1ff87add9
lindabettini <linda.bettini@hotmail.it>
2022-03-28 19:31:10 +0200

    add form delete

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -11,8 +11,25 @@
 <time>{{ $post->created_at }}</time>
 <hr>
 <div class="d-flex align-items-center justify-content-end">
-  <a href="{{route('admin.posts.index')}}" class="btn btn-secondary">
+  <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST" id="delete-form">
+    @method('DELETE')
+    @csrf
+    <button type="submit" class="btn btn-danger">Elimina</button>
+  </form>
+
+  <a href="{{route('admin.posts.index')}}" class="btn btn-secondary ml-2">
     <i class="fa-solid fa-arrow-left mr-2"></i></i>Indietro
   </a>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+  const deleteForm = document.getElementById('delete-form');
+  deleteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const confirmation = confirm('Sei sicuro di voler eliminare questo post?');
+    if (confirmation) e.target.submit();
+  });
+</script>
 @endsection
